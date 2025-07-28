588c888e76c4f35e38c3e3620fbedc07c044ff6d
linuxdeda <linuxdeda@gmail.com>
2025-07-28 10:52:14 +0200

    First flake commit, linuxdeda.

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NixOS flake sa Home Manager-om";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations.nixos-flakes = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./configuration.nix
+
+        # Omogućavamo Home Manager kao sistemski modul
+        home-manager.nixosModules.home-manager
+
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.linuxdeda = import ./home.nix;
+        }
+      ];
+    };
+  };
+}

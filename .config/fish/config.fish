1e3276d09fe6f95b3f642627d327dd268500b4fc
linuxdeda <linuxdeda@gmail.com>
2026-01-09 18:44:02 +0100

    commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+set -U fish_greeting
+
+starship init fish | source
+
+function banner
+  set COLS (tput cols)
+  printf '\e[H\e[2J'
+  figlet -f standard -w $COLS "linuxdeda.com"
+  echo
+end
+
+banner; fastfetch; echo
+
+bind \cl 'banner; commandline -f repaint'
+
